0eeda8e8540b817b6a52a557d42c628b918efa55
Jagannath Nayak <jagannathnayak2105@gmail.com>
2023-08-11 17:01:14 +0530

    minor change

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
       <h3 className="text-4xl md:text-4xl text-gray-300 font-bold capitalize mb-10">My Projects</h3>
       </div>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 place-content-center justify-items-center">
+      <div className="mx-4 lg:mx-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-14 place-content-center justify-items-center">
         {projects.map((project)=>(
             <ProjectsCard title={project.title} des={project.des} src={project.src} git={project.git} link={project.link}/>
         ))}
